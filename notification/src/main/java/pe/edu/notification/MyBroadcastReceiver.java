7b60f890dcddc21b54ac0b621096973037f232d0
LuisDCL-714 <luisdanie7calderon@gmail.com>
2019-05-26 11:25:52 -0500

    Proyecto de Broadcast y Notification, se agrego la notificación

--- notification/src/main/java/pe/edu/notification/MyBroadcastReceiver.java
+++ notification/src/main/java/pe/edu/notification/MyBroadcastReceiver.java
@@ -1,19 +1,37 @@
 package pe.edu.notification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     private String action;
+    private Intent newIntent;
+    private PendingIntent pendingIntent;
+    private NotificationCompat.Builder notification;
+    private NotificationManagerCompat managerCompat;
     @Override
     public void onReceive(Context context, Intent intent) {
         action = intent.getAction();
-
         if(action.equals("pe.edu.cibertec.broadcast.ACTION")){
-            Toast.makeText(context, "Llego una notificación", Toast.LENGTH_LONG).show();
-        }
+            newIntent = new Intent(context, NotificationReceiverActivity.class);
 
+            pendingIntent = PendingIntent.getActivity(context, 1, newIntent, 0);
+            notification = new NotificationCompat.Builder(context, "Canal")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Notificación nueva")
+            .setContentText("Esto es una nueva notificación")
+            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent);
+
+            managerCompat = NotificationManagerCompat.from(context);
+            managerCompat.notify(0, notification.build());
+
+        }
     }
 }
